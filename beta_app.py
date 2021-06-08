2bc0b4298812588f7399521ac30d61b8b9b2c97a
Rowan Molony <rowan.molony@codema.ie>
2021-06-08 15:49:17 +0100

    Refactor load_data to multiple functions (#17)

--- beta_app.py
+++ beta_app.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import Any, Dict
 from typing import List
 from typing import Tuple
+from altair.vegalite.v4.schema.core import Data
 
 import geopandas as gpd
 from geopandas import GeoDataFrame
@@ -24,46 +25,57 @@ LOCAL_AUTHORITIES = [
     "Fingal",
     "Dún Laoghaire-Rathdown",
 ]
-INDEX_COLUMNS = ["SMALL_AREA", "dwelling_type", "period_built", "category_id"]
 
 
 @st.cache
-def load_data(
-    data_dir: Path,
-    index_columns: List[str],
-) -> Tuple[Any]:
-
+def load_building_stock(data_dir: Path) -> Tuple[DataFrame]:
+    index_columns = ["SMALL_AREA", "dwelling_type", "period_built", "category_id"]
     known_indiv_hh = pd.read_parquet(
         data_dir / "dublin_indiv_hhs_known.parquet"
-    ).set_index(INDEX_COLUMNS)
-
+    ).set_index(index_columns)
     unknown_indiv_hh = pd.read_parquet(
         data_dir / "dublin_indiv_hhs_unknown.parquet"
-    ).set_index(INDEX_COLUMNS)
+    ).set_index(index_columns)
+    return known_indiv_hh, unknown_indiv_hh
 
-    small_area_boundaries = gpd.read_file(
+
+@st.cache
+def load_geodata(data_dir: Path) -> Tuple[GeoDataFrame]:
+    return gpd.read_file(
         data_dir / "dublin_small_area_boundaries_2011.geojson", driver="GeoJSON"
     ).to_crs(epsg=4326)
 
-    with open(DATA_DIR / "electoral_districts.json", "r") as file:
-        electoral_districts = [None] + json.load(file)
 
-    wall_archetypes = pd.read_csv(
+@st.cache
+def load_archetypes(data_dir: Path) -> Tuple[DataFrame]:
+
+    wall_type_archetypes = pd.read_csv(
         data_dir / "most_common_wall_types_by_archetype.csv",
     ).set_index(["dwelling_type", "period_built"])
+
+    wall_uvalue_defaults = pd.read_csv(
+        data_dir / "deap-default-wall-uvalues-by-period-built.csv"
+    ).set_index(["most_significant_wall_type", "period_built"])
 
     with open(data_dir / "archetype_new_build.json", "r") as file:
         new_build_archetype = pd.Series(json.load(file))
 
+    return (
+        wall_type_archetypes,
+        wall_uvalue_defaults,
+        new_build_archetype,
+    )
+
+
+@st.cache
+def load_misc_data(data_dir: Path) -> Tuple[Any]:
+    with open(DATA_DIR / "electoral_districts.json", "r") as file:
+        electoral_districts = [None] + json.load(file)
+
     esri_forecast = pd.read_csv(data_dir / "esri_housing_forecast_12_2020.csv")
 
     return (
-        known_indiv_hh,
-        unknown_indiv_hh,
-        small_area_boundaries,
         electoral_districts,
-        wall_archetypes,
-        new_build_archetype,
         esri_forecast,
     )
 
@@ -80,15 +92,22 @@ def _convert_categorical_columns_to_str(df: DataFrame) -> DataFrame:
 data_dir = Path("./data")
 
 st.title("Dublin Housing Energy Demand App")
+
+## Load data.
 (
     known_indiv_hh,
     unknown_indiv_hh,
-    small_area_boundaries,
-    electoral_districts,
-    wall_archetypes,
+) = load_building_stock(data_dir)
+small_area_boundaries = load_geodata(data_dir)
+(
+    wall_type_archetypes,
+    wall_uvalue_defaults,
     new_build_archetype,
+) = load_archetypes(data_dir)
+(
+    electoral_districts,
     esri_forecast,
-) = load_data(data_dir, INDEX_COLUMNS)
+) = load_misc_data(data_dir)
 
 if st.checkbox("Show sample of known building stock?"):
     text = (
@@ -107,11 +126,13 @@ if st.checkbox("Show wall archetypes?"):
         "<strong>Source:</strong> Derived from SEAI's BER public dataset (June, 2021)"
     )
     st.markdown(text, unsafe_allow_html=True)
-    st.write(wall_archetypes.reset_index().pipe(_convert_categorical_columns_to_str))
+    st.write(
+        wall_type_archetypes.reset_index().pipe(_convert_categorical_columns_to_str)
+    )
 
 wall_types = archetypes.estimate_type_of_wall(
     known_indiv_hh,
     unknown_indiv_hh,
-    wall_archetypes,
-    on=wall_archetypes.index.names,
+    wall_type_archetypes,
+    on=wall_type_archetypes.index.names,
 )
