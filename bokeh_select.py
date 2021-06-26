44b3999ce56a3f10445a73a0a85f876bd1693af0
Oisin Doherty <oisin.doherty@codema.ie>
2021-06-26 22:30:43 +0100

    Add plot of dublin sa points

--- bokeh_select.py
+++ bokeh_select.py
@@ -3,50 +3,53 @@ from typing import List
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource, CustomJS
 from bokeh.models import DataTable, TableColumn
+from bokeh.io import output_notebook
 
 import geopandas as gpd
 import streamlit as st
 from streamlit_bokeh_events import streamlit_bokeh_events
+
+output_notebook()
 
 
 def select_small_areas(small_area_boundaries: gpd.GeoDataFrame) -> List[str]:
     small_area_boundaries["x"] = small_area_boundaries.geometry.centroid.x
     small_area_boundaries["y"] = small_area_boundaries.geometry.centroid.y
     small_area_boundaries = small_area_boundaries.drop(columns="geometry")
-    p = figure(tools="lasso_select")
-    cds = ColumnDataSource(small_area_boundaries)
-    p.circle("x", "y", source=cds)
 
-    # define events
-    cds.selected.js_on_change(
+    cds_lasso = ColumnDataSource(small_area_boundaries)
+    cds_lasso.selected.js_on_change(
         "indices",
         CustomJS(
-            args=dict(source=cds),
+            args=dict(source=cds_lasso),
             code="""
-            document.dispatchEvent(
-                new CustomEvent("YOUR_EVENT_NAME", {detail: {your_data: "goes-here"}})
-            )
-            """,
+        document.dispatchEvent(
+            new CustomEvent("LASSO_SELECT", {detail: {data: source.selected.indices}})
+        )
+        """,
         ),
     )
 
-    # result will be a dict of {event_name: event.detail}
-    # events by default is "", in case of more than one events pass it as a comma separated values
-    # event1,event2
-    # debounce is in ms
-    # refresh_on_update should be set to False only if we dont want to update datasource at runtime
-    # override_height overrides the viewport height
-    result = streamlit_bokeh_events(
-        bokeh_plot=p,
-        events="YOUR_EVENT_NAME",
-        key="foo",
-        refresh_on_update=False,
-        override_height=600,
-        debounce_time=500,
+    plot = figure(
+        tools="pan, zoom_in, zoom_out, box_zoom, wheel_zoom, lasso_select",
+        width=250,
+        height=250,
     )
 
-    # use the result
-    st.write(result)
+    plot.circle("x", "y", fill_alpha=0.5, size=5, source=cds_lasso)
+    result_lasso = streamlit_bokeh_events(
+        bokeh_plot=plot,
+        events="LASSO_SELECT",
+        key="bar",
+        refresh_on_update=False,
+        debounce_time=0,
+    )
+    if result_lasso:
+        if result_lasso.get("LASSO_SELECT"):
+            df_extract = small_area_boundaries.iloc[
+                result_lasso.get("LASSO_SELECT")["data"]
+            ]
+            st.write(df_extract.sum())
 
 
 if __name__ == "__main__":
