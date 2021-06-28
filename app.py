5239624cfb59f9bd97ccc27555872f38f0227205
Rowan Molony <rowan.molony@codema.ie>
2021-06-28 15:09:54 +0100

    Change default BER Band from A-B to All

--- app.py
+++ app.py
@@ -186,8 +186,7 @@ def filter_by_ber_level(building_stock: pd.DataFrame) -> pd.DataFrame:
     options = ["All"] + ber_bands
     selected_ber_rating = st.selectbox(
         "Filter by BER Rating: ",
-        options,
-        index=1,
+        options,,
     )
     if selected_ber_rating == "All":
         mask = pd.Series([True] * len(building_stock), dtype="bool")
