63ad8dcf3861d2b990274e497d77f24726310801
Rowan Molony <rowan.molony@codema.ie>
2021-06-29 17:17:24 +0100

    Refactor into plot & ask_user
    As the main has 2 different jobs:
    - plot map
    - get user input from map selection

--- bokeh_select.py
+++ bokeh_select.py
@@ -1,55 +1,78 @@
 from typing import List
+from bokeh.models.plots import Plot
 
 from bokeh.plotting import figure
+from bokeh.plotting import Figure
 from bokeh.models import ColumnDataSource, CustomJS
-from bokeh.models import DataTable, TableColumn
-from bokeh.io import output_notebook
 
 import geopandas as gpd
+import pandas as pd
 import streamlit as st
 from streamlit_bokeh_events import streamlit_bokeh_events
 
-output_notebook()
+
+def _convert_geometry_to_xy(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    return gdf.assign(
+        x=lambda gdf: gdf.geometry.centroid.x, y=lambda gdf: gdf.geometry.centroid.y
+    ).drop(columns="geometry")
 
 
-def select_small_areas(small_area_boundaries: gpd.GeoDataFrame) -> List[str]:
-    small_area_boundaries["x"] = small_area_boundaries.geometry.centroid.x
-    small_area_boundaries["y"] = small_area_boundaries.geometry.centroid.y
-    small_area_boundaries = small_area_boundaries.drop(columns="geometry")
-
-    cds_lasso = ColumnDataSource(small_area_boundaries)
+def _plot_small_area_points(small_area_points) -> Figure:
+    cds_lasso = ColumnDataSource(small_area_points)
     cds_lasso.selected.js_on_change(
         "indices",
         CustomJS(
             args=dict(source=cds_lasso),
             code="""
-        document.dispatchEvent(
-            new CustomEvent("LASSO_SELECT", {detail: {data: source.selected.indices}})
-        )
-        """,
+            document.dispatchEvent(
+                new CustomEvent("LASSO_SELECT", {detail: {data: source.selected.indices}})
+            )
+            """,
         ),
     )
-
     plot = figure(
         tools="pan, zoom_in, zoom_out, box_zoom, wheel_zoom, lasso_select",
-        width=250,
-        height=250,
+        width=500,
+        height=500,
     )
-
     plot.circle("x", "y", fill_alpha=0.5, size=5, source=cds_lasso)
-    result_lasso = streamlit_bokeh_events(
-        bokeh_plot=plot,
+    return plot
+
+
+def _ask_user_for_small_areas(
+    bokeh_plot: Plot, small_area_points: gpd.GeoDataFrame
+) -> pd.Series:
+    lasso_selected = streamlit_bokeh_events(
+        bokeh_plot=bokeh_plot,
         events="LASSO_SELECT",
         key="bar",
         refresh_on_update=False,
         debounce_time=0,
     )
-    if result_lasso:
-        if result_lasso.get("LASSO_SELECT"):
-            df_extract = small_area_boundaries.iloc[
-                result_lasso.get("LASSO_SELECT")["data"]
-            ]
-            st.write(df_extract.sum())
+    if lasso_selected:
+        try:
+            indices_selected = lasso_selected.get("LASSO_SELECT")["data"]
+        except:
+            raise ValueError("No Small Areas selected!")
+        small_areas_selected = small_area_points.iloc[indices_selected]["small_area"]
+    else:
+        small_areas_selected = small_area_points["small_area"]
+    return small_areas_selected
+
+
+def select_small_areas(small_area_boundaries: gpd.GeoDataFrame) -> List[str]:
+    small_area_points = _convert_geometry_to_xy(
+        small_area_boundaries.set_crs(epsg="2157").to_crs(epsg="3857")
+    )  # CARTODBPOSITRON requires this crs
+    bokeh_plot = _plot_small_area_points(small_area_points)
+    st.subheader("Select Dwellings by Small Area")
+    st.markdown("> Click on the `Lasso Select` tool on the toolbar below!")
+    small_areas_selected = _ask_user_for_small_areas(
+        bokeh_plot=bokeh_plot, small_area_points=small_area_points
+    )
+    with st.beta_expander("Show Selected Small Areas"):
+        st.write("Small Areas: " + str(small_areas_selected.to_list()))
+    return small_areas_selected
 
 
 if __name__ == "__main__":
--- test_bokeh_select.py
+++ test_bokeh_select.py
@@ -1,17 +1,24 @@
 import geopandas as gpd
-from geopandas.testing import assert_geodataframe_equal
+import pandas as pd
+from pandas.testing import assert_series_equal
 from shapely.geometry import Point
 
 import bokeh_select
 
 
-def test_select_small_areas():
+def test_select_small_areas(monkeypatch):
+    def _mock_ask_user_for_small_areas(*args, **kwargs):
+        return pd.Series(["1", "2", "3"])
+
+    monkeypatch.setattr(
+        "bokeh_select._ask_user_for_small_areas", _mock_ask_user_for_small_areas
+    )
     small_area_boundaries = gpd.GeoDataFrame(
         {
             "small_area": ["1", "2", "3"],
             "geometry": [Point(0, 0), Point(0, 1), Point(1, 1)],
         }
     )
-    expected_output = small_area_boundaries
+    expected_output = pd.Series(["1", "2", "3"])
     output = bokeh_select.select_small_areas(small_area_boundaries)
-    assert_geodataframe_equal(output, expected_output)
+    assert_series_equal(output, expected_output)
