0eef08f917f49e27414a83c7c70e864c3bc962e9
Rowan Molony <rowan.molony@codema.ie>
2021-07-09 12:46:30 +0100

    Save Small Area Bldgs & Boundaries to s3
    - Store URLs in a config.ini file
    - Store s3 authentication in .streamlit/secrets.toml which lets the
    streamlit app access private data
    - Cache the data locally using fsspec on first read to avoid multiple
    calls to s3 during local development

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from configparser import ConfigParser
 import json
+from os import getenv
 from pathlib import Path
+from typing import Callable
 from typing import Dict, List, Union
 
 import altair as alt
@@ -8,6 +11,7 @@ from bokeh.plotting import figure
 from bokeh.plotting import Figure
 from bokeh.models import ColumnDataSource, CustomJS, GeoJSONDataSource
 from bokeh.tile_providers import CARTODBPOSITRON, get_provider
+import fsspec
 import geopandas as gpd
 import numpy as np
 import pandas as pd
@@ -16,6 +20,8 @@ from rc_building_model import htuse
 import streamlit as st
 from streamlit_bokeh_events import streamlit_bokeh_events
 
+from dublin_energy_app import CONFIG
+from dublin_energy_app import _DATA_DIR
 
 data_dir = Path("data")
 
@@ -40,12 +46,14 @@ EXPECTED_COLUMNS = [
 ]
 
 
-def main():
+def main(data_dir: Path = _DATA_DIR, config: ConfigParser = CONFIG):
     st.header("Welcome to the Dublin Retrofitting Tool")
 
     ## Load
-    raw_building_stock = load_bers()
-    small_area_boundaries = load_small_area_boundaries()
+    raw_building_stock = _load_bers(url=config["urls"]["bers"], data_dir=data_dir)
+    small_area_boundaries = _load_small_area_boundaries(
+        url=config["urls"]["small_area_boundaries"], data_dir=data_dir
+    )
     small_area_points = _convert_gdf_geometry_to_xy(small_area_boundaries)
     small_area_boundaries_geojson = _convert_to_geojson_str(small_area_boundaries)
 
@@ -157,26 +165,32 @@ def main():
     )
 
 
+def _load(read: Callable, url: str, data_dir: Path, filesystem_name: str):
+    filename = url.split("/")[-1]
+    filepath = data_dir / filename
+    if not filepath.exists():
+        fs = fsspec.filesystem(filesystem_name)
+        with fs.open(url, cache_storage=filepath) as f:
+            df = read(f)
+    else:
+        df = read(filepath)
+    return df
+
+
 @st.cache
-def fetch_bers():
-    bers = pd.read_csv("https://storage.googleapis.com/codema-dev/bers.csv")
+def _load_bers(url: str, data_dir: Path):
+    bers = _load(read=pd.read_parquet, url=url, data_dir=data_dir, filesystem_name="s3")
     assert set(EXPECTED_COLUMNS).issubset(bers.columns)
     return bers
 
 
 @st.cache
-def load_bers():
-    bers = pd.read_csv("data/dublin_bers.csv")
-    assert set(EXPECTED_COLUMNS).issubset(bers.columns)
-    return bers
-
-
-@st.cache
-def load_small_area_boundaries() -> gpd.GeoDataFrame:
+def _load_small_area_boundaries(url: str, data_dir: Path) -> gpd.GeoDataFrame:
+    boundaries = _load(
+        read=gpd.read_parquet, url=url, data_dir=data_dir, filesystem_name="s3"
+    )
     # CARTODBPOSITRON tile requires this crs
-    return gpd.read_file(
-        "data/dublin_small_area_boundaries_2016",
-    ).to_crs(epsg="3857")
+    return boundaries.to_crs(epsg="3857")
 
 
 @st.cache
