bb01c83c9cd939462dc1ac5df49a3a8aea22fd1f
Rowan Molony <rowan.molony@codema.ie>
2021-07-12 14:52:40 +0100

    Add retrofit target/cost selections
    - Fill defaults from a json file - so code and config is kept separate
    - Retrofit selections are now indendent of the data and so can be run
    on click

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from configparser import ConfigParser
 from pathlib import Path
+from typing import Any
+from typing import Dict
+from typing import Union
 
 import geopandas as gpd
 import pandas as pd
@@ -9,10 +12,17 @@ from dea import filter
 from dea import io
 from dea.mapselect import mapselect
 from dea import CONFIG
+from dea import DEFAULTS
 from dea import _DATA_DIR
 
+DeaSelection = Dict[str, Dict[str, Union[int, Dict[str, int]]]]
 
-def main(data_dir: Path = _DATA_DIR, config: ConfigParser = CONFIG):
+
+def main(
+    defaults: DeaSelection = DEFAULTS,
+    data_dir: Path = _DATA_DIR,
+    config: ConfigParser = CONFIG,
+):
     st.header("Welcome to the Dublin Retrofitting Tool")
 
     ## Load
@@ -31,6 +41,7 @@ def main(data_dir: Path = _DATA_DIR, config: ConfigParser = CONFIG):
         selections["small_area"] = mapselect(
             column_name="small_area", boundaries=small_area_boundaries
         )
+        selections["retrofit"] = _retrofitselect(defaults)
         inputs_are_submitted = st.form_submit_button(label="Submit")
 
     if inputs_are_submitted:
@@ -48,6 +59,43 @@ def main(data_dir: Path = _DATA_DIR, config: ConfigParser = CONFIG):
             selected_substrings=selections["small_area"],
             all_substrings=small_area_boundaries["small_area"],
         )
+
+
+def _retrofitselect(defaults: DeaSelection) -> DeaSelection:
+    selections = defaults.copy()
+    with st.beta_expander(label="Change Default Costs & Threshold Values"):
+        for component, properties in defaults.items():
+            selections[component]["uvalue"]["target"] = st.number_input(
+                label="Threshold U-Value [W/m²K] - assume no retrofits below this value",
+                min_value=float(0),
+                value=properties["uvalue"]["target"],
+                key=component,
+                step=0.05,
+            )
+            c1, c2 = st.beta_columns(2)
+            selections[component]["cost"]["lower"] = c1.number_input(
+                label="Lowest* Likely Cost [€/m²]",
+                min_value=0,
+                value=properties["cost"]["lower"],
+                key=component,
+                step=5,
+            )
+            selections[component]["cost"]["upper"] = c2.number_input(
+                label="Highest** Likely Cost [€/m²]",
+                min_value=0,
+                value=properties["cost"]["upper"],
+                key=component,
+                step=5,
+            )
+            footnote = f"""
+                <small> * {properties["typical_area"] * properties["cost"]["lower"]}€
+                for a typical {component} area of {properties["typical_area"]}m²<br>
+                ** {properties["typical_area"]  * properties["cost"]["upper"]}€
+                for a typical {component} area of {properties["typical_area"]}m²</small>
+                """
+            st.markdown(footnote, unsafe_allow_html=True)
+
+    return selections
 
 
 @st.cache
--- dea/__init__.py
+++ dea/__init__.py
@@ -1,4 +1,5 @@
 from configparser import ConfigParser
+import json
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -10,3 +11,6 @@ _DATA_DIR = Path(__file__).parent.parent / "data"
 
 CONFIG = ConfigParser()
 CONFIG.read(_SRC_DIR / "config.ini")
+
+with open(_SRC_DIR / "defaults.json") as f:
+    DEFAULTS = json.load(f)
